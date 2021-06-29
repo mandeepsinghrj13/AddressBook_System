f0c3c06b3fbc9f447d83b4c7d5c6b91e6ebfdb8d
mandeep <singh.mr.1996@gmail.com>
2021-06-29 17:29:34 +0530

    create a contacts in addressbook with varibles

--- src/com/bridgelabz/AddressBook.java
+++ src/com/bridgelabz/AddressBook.java
@@ -3,9 +3,69 @@ package com.bridgelabz;
 public class AddressBook 
 {
 
-	public static void main(String[] args) 
-	{
-		System.out.println("Welcome to address book");
+	private String firstname;
+	private String lastname;
+	private String city;
+	private String state;
+	private String zip;
+	private String phoneNumber;
+	private String email;
+	
+	public String getFirstname() {
+		return firstname;
 	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	
+	
+	public String getZip() {
+		return zip;
+	}
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	@Override
+	public String toString() {
+		return "AddressBook [firstname=" + firstname + ", lastname=" + lastname + ", city=" + city + ", state=" + state
+				+ ", zip=" + zip + ", phoneNumber=" + phoneNumber + ", email=" + email + "]";
+	}
+
+    public static void main(String[] args) {
+         System.out.println("Welcome to Address Book Program....!!!");
+     	 AddressBook addressbook = new AddressBook();
+		 AddressBook addressbook1 = new AddressBook();
+    }
 
 }
