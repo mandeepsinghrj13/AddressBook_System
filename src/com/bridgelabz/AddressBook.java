bef15ee5d24b2df95e953bee6cb53ea183daa581
mandeep <singh.mr.1996@gmail.com>
2021-07-09 20:58:09 +0530

    edit person details

--- src/com/bridgelabz/AddressBook.java
+++ src/com/bridgelabz/AddressBook.java
@@ -3,7 +3,6 @@ import java.util.ArrayList;
 import java.util.Scanner;
 public class AddressBook 
 {
-
 	private String firstname;
 	private String lastname;
 	private String city;
@@ -12,120 +11,104 @@ public class AddressBook
 	private String phoneNumber;
 	private String email;
 	
-	public String getFirstname() 
-	{
+	public String getFirstname() {
 		return firstname;
 	}
-	public void setFirstname(String firstname) 
-	{
+	public void setFirstname(String firstname) {
 		this.firstname = firstname;
 	}
-	public String getLastname() 
-	{
+	public String getLastname() {
 		return lastname;
 	}
-	public void setLastname(String lastname) 
-	{
+	public void setLastname(String lastname) {
 		this.lastname = lastname;
 	}
-	public String getCity() 
-	{
+	public String getCity() {
 		return city;
 	}
-	public void setCity(String city) 
-	{
+	public void setCity(String city) {
 		this.city = city;
 	}
-	public String getState() 
-	{
+	public String getState() {
 		return state;
 	}
-	public void setState(String state) 
-	{
+	public void setState(String state) {
 		this.state = state;
 	}
 	
 	
-	public String getZip() 
-	{
+	public String getZip() {
 		return zip;
 	}
-	public void setZip(String zip) 
-	{
+	public void setZip(String zip) {
 		this.zip = zip;
 	}
-	public String getPhoneNumber() 
-	{
+	public String getPhoneNumber() {
 		return phoneNumber;
 	}
-	public void setPhoneNumber(String phoneNumber) 
-	{
+	public void setPhoneNumber(String phoneNumber) {
 		this.phoneNumber = phoneNumber;
 	}
-	public String getEmail() 
-	{
+	public String getEmail() {
 		return email;
 	}
-	public void setEmail(String email) 
-	{
+	public void setEmail(String email) {
 		this.email = email;
 	}
 	
 	@Override
-	public String toString() 
-	{
+	public String toString() {
 		return "AddressBook [firstname=" + firstname + ", lastname=" + lastname + ", city=" + city + ", state=" + state
 				+ ", zip=" + zip + ", phoneNumber=" + phoneNumber + ", email=" + email + "]";
 	}
 
-    public static void main(String[] args) 
-    {
+    public static void main(String[] args) {
          System.out.println("Welcome to Address Book Program....!!!");
-     	 AddressBook addressbook = new AddressBook();
-		 AddressBook addressbook1 = new AddressBook();
-		 ArrayList<AddressBook> p=new ArrayList<AddressBook>();
- 		 ArrayList<AddressBook> totalarray=new ArrayList<AddressBook>();
- 		 Scanner sc = new Scanner(System.in);
- 			 System.out.println("Enter your name:");
- 			 String s=sc.nextLine();
- 			 addressbook.setFirstname(s);
- 			 String fname= addressbook. getFirstname();
- 			 System.out.println(fname);
- 		
- 			 System.out.println("Enter your Last Name:");
- 			 addressbook.setLastname(sc.nextLine());
- 			 String lname= addressbook.getLastname();
- 			 System.out.println(lname);
- 		 
- 			 System.out.println("Enter your City:");
- 			 addressbook.setCity(sc.nextLine());
- 			 String city= addressbook. getCity();
- 			 System.out.println(city);
- 		
- 			 System.out.println("Enter your State:");
- 			 addressbook.setState(sc.nextLine());
- 			 String state= addressbook. getState();
- 			 System.out.println(state);
- 				 
- 			 System.out.println("Enter your Zip:");
- 			 addressbook.setZip(sc.next());
- 			 String zip= addressbook. getZip();
- 			 System.out.println(zip);
- 				
- 			 System.out.println("Enter your PhoneNumber:");
- 			 addressbook.setPhoneNumber(sc.next());
- 			 String  PhoneNumber= addressbook.getPhoneNumber();
- 			 System.out.println( PhoneNumber);
- 					 
- 			 System.out.println("Enter your email:");
- 			 addressbook.setEmail(sc.next());
- 			 String email= addressbook. getEmail();
- 			 System.out.println( email );
- 			 
- 			 
- 			 System.out.println("Enter your name:");
+     	AddressBook addressbook = new AddressBook();
+		AddressBook addressbook1 = new AddressBook();
+		ArrayList<AddressBook> l1=new ArrayList<AddressBook>();
+		ArrayList<AddressBook> l=new ArrayList<AddressBook>();
+		 Scanner sc = new Scanner(System.in);
+			 System.out.println("Enter your name:");
+			 String s=sc.nextLine();
+			 addressbook.setFirstname(s);
+			 String fname= addressbook. getFirstname();
+			 System.out.println(fname);
+		
+			 System.out.println("Enter your Last Name:");
+			 addressbook.setLastname(sc.nextLine());
+			 String lname= addressbook.getLastname();
+			 System.out.println(lname);
+		 
+			 System.out.println("Enter your City:");
+			 addressbook.setCity(sc.nextLine());
+			 String city= addressbook. getCity();
+			 System.out.println(city);
+		
+			 System.out.println("Enter your State:");
+			 addressbook.setState(sc.nextLine());
+			 String state= addressbook. getState();
+			 System.out.println(state);
+				 
+			 System.out.println("Enter your Zip:");
+			 addressbook.setZip(sc.next());
+			 String zip= addressbook. getZip();
+			 System.out.println(zip);
+				
+			 System.out.println("Enter your PhoneNumber:");
+			 addressbook.setPhoneNumber(sc.next());
+			 String  PhoneNumber= addressbook.getPhoneNumber();
+			 System.out.println( PhoneNumber);
+					 
+			 System.out.println("Enter your email:");
+			 addressbook.setEmail(sc.next());
+			 String email= addressbook. getEmail();
+			 System.out.println(email);
+			 
+		
+			 System.out.println("Enter your name:");
 			 String s1=sc.nextLine();
-			 addressbook1.setFirstname(sc.nextLine());
+			 addressbook1.setFirstname(s1);
 			 String fname1= addressbook1. getFirstname();
 			 System.out.println(fname1);
 		
@@ -156,12 +139,28 @@ public class AddressBook
 					 
 			 System.out.println("Enter your email:");
 			 addressbook1.setEmail(sc.next());
-			 String email1= addressbook1. getEmail();
+			 String email1= addressbook1.getEmail();
 			 System.out.println(email1);
- 			p.add(addressbook);
- 			p.add(addressbook1);
- 			totalarray.addAll(p);
- 			System.out.println(""+totalarray );
-    }
-
+			
+		 
+			 l.add(addressbook);
+			 l.add(addressbook1);
+			 l1.addAll(l);
+			 System.out.println("All list"+l1);
+		 
+			 System.out.println("Edit record");
+			 System.out.println("Enter name to edit:");
+			 for(int m=0;m<l1.size();m++){
+				 addressbook=l1.get(m);
+			 if(sc.nextLine().equals(addressbook.firstname)) {
+				 System.out.println("Enter new city:");
+				 addressbook.setCity(sc.nextLine());
+				 System.out.println("List After is"+l1.toString());
+				 break;
+			 }else {
+				 System.out.println("User not found");
+			 }
+		}
+		
+   }
 }
