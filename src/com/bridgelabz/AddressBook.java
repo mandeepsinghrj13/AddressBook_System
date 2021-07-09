dff6a54d67c3de94b5baac3ef05b8b6254e57ad8
mandeep <singh.mr.1996@gmail.com>
2021-07-09 21:08:40 +0530

    address book has unique name

--- src/com/bridgelabz/AddressBook.java
+++ src/com/bridgelabz/AddressBook.java
@@ -1,5 +1,6 @@
 package com.bridgelabz;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 public class AddressBook 
 {
@@ -49,14 +50,14 @@ public class AddressBook
 	public String getPhoneNumber() {
 		return phoneNumber;
 	}
-	public void setPhoneNumber(String string) {
-		this.phoneNumber = string;
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber =phoneNumber;
 	}
 	public String getEmail() {
 		return email;
 	}
 	public void setEmail(String email) {
-		this.email = email;
+		this.email =email;
 	}
 	
 	@Override
@@ -67,122 +68,135 @@ public class AddressBook
 	public static void main(String[] args) {
 	   
 	   System.out.println("Welcome to Address Book Program....!!!");
-	
+	   User();
+		
+	}
+	public static void User() {
 		try {
-		AddressBook addressbook = new AddressBook();
-		AddressBook addressbook1 = new AddressBook();
-		ArrayList<AddressBook> l1=new ArrayList<AddressBook>();
-		ArrayList<AddressBook> l=new ArrayList<AddressBook>();
-		 Scanner sc = new Scanner(System.in);
-			 System.out.println("Enter your name:");
-			 String s=sc.nextLine();
-			 addressbook.setFirstname(s);
-			 String fname= addressbook. getFirstname();
-			 System.out.println(fname);
-		
-			 System.out.println("Enter your Last Name:");
-			 addressbook.setLastname(sc.nextLine());
-			 String lname= addressbook.getLastname();
-			 System.out.println(lname);
-		 
-			 System.out.println("Enter your City:");
-			 addressbook.setCity(sc.nextLine());
-			 String city= addressbook. getCity();
-			 System.out.println(city);
-		
-			 System.out.println("Enter your State:");
-			 addressbook.setState(sc.nextLine());
-			 String state= addressbook. getState();
-			 System.out.println(state);
-				 
-			 System.out.println("Enter your Zip:");
-			 addressbook.setZip(sc.next());
-			 String zip= addressbook. getZip();
-			 System.out.println(zip);
-				
-			 System.out.println("Enter your PhoneNumber:");
-			 addressbook.setPhoneNumber(sc.next());
-			 String  PhoneNumber= addressbook.getPhoneNumber();
-			 System.out.println( PhoneNumber);
-					 
-			 System.out.println("Enter your email:");
-			 addressbook.setEmail(sc.nextLine());
-			 String email= addressbook. getEmail();
-			 System.out.println(email);
-			 
-		
-			 System.out.println("Enter your name:");
-			 String s1=sc.nextLine();
-			 addressbook1.setFirstname(s1);
-			 String fname1= addressbook1. getFirstname();
-			 System.out.println(fname1);
-		
-			 System.out.println("Enter your Last Name:");
-			 addressbook1.setLastname(sc.nextLine());
-			 String lname1= addressbook1.getLastname();
-			 System.out.println(lname1);
-		 
-			 System.out.println("Enter your City:");
-			 addressbook1.setCity(sc.nextLine());
-			 String city1= addressbook1. getCity();
-			 System.out.println(city1);
-		
-			 System.out.println("Enter your State:");
-			 addressbook1.setState(sc.nextLine());
-			 String state1= addressbook1. getState();
-			 System.out.println(state1);
-				 
-			 System.out.println("Enter your Zip:");
-			 addressbook1.setZip(sc.next());
-			 String zip1= addressbook1. getZip();
-			 System.out.println(zip1);
-			 
-			 System.out.println("Enter your PhoneNumber:");
-			 addressbook1.setPhoneNumber(sc.next());
-			 String  PhoneNumber1= addressbook1.getPhoneNumber();
-			 System.out.println( PhoneNumber1);
-					 
-			 System.out.println("Enter your email:");
-			 addressbook1.setEmail(sc.nextLine());
-			 String email1= addressbook1. getEmail();
-			 System.out.println(email1);
 			
-		 
-		 l.add(addressbook);
-		 l.add(addressbook1);
-		 l1.addAll(l);
-		 System.out.println("All list"+l1);
-		
-		System.out.println("Edit record");
-		 System.out.println("Enter name to edit:");
-		for(int m=0;m<l1.size();m++){
-			addressbook=l1.get(m);
-			 if(sc.nextLine().equals(addressbook.firstname)) {
-				 System.out.println("Enter new city:");
-				 addressbook.setCity(sc.nextLine());
-				 System.out.println("List After is"+l1.toString());
-				 break;
-			 }else {
-				 System.out.println("User not found");
-			 }
-		}
-		
-		System.out.println("remove record");
-		 System.out.println("Enter name to remove:");
-		for(int m=0;m<l1.size();m++){
-			addressbook=l1.get(m);
-			 if(sc.nextLine().equals(addressbook.firstname)) {
-				 l1.remove(addressbook);
-				 System.out.println("List After removing"+l1.toString());
-				 break;
-			 }else {
-				 System.out.println("User not found");
-			 }
-		}			
-		 sc.close();
-		}catch(Exception e){
+			//AddressBook addressbook1 = new AddressBook();
+			ArrayList<AddressBook> l=new ArrayList<AddressBook>();
+			
+			Scanner sc = new Scanner(System.in);
+			 	for(int i = 0; i < 2; i++) {
+			 		AddressBook addressbook = new AddressBook();
+				 System.out.println("Enter your name:");
+			     addressbook.setFirstname(sc.next());
+			     
+				 System.out.println("Enter your Last Name:");
+				 addressbook.setLastname(sc.next());
+			 
+				 System.out.println("Enter your City:");
+				 addressbook.setCity(sc.next());
+			
+				 System.out.println("Enter your State:");
+				 addressbook.setState(sc.next());
+				 
+				 System.out.println("Enter your Zip:");
+				 addressbook.setZip(sc.next());
+				 
+				 System.out.println("Enter your PhoneNumber:");
+				 addressbook.setPhoneNumber(sc.next());
+						 
+				 System.out.println("Enter your email:");
+				 addressbook.setEmail(sc.next());
+				 l.add(addressbook);
+			 	}	
+			 	
+			 	System.out.println("All list" +l.toString());
+			 	boolean choice=true;
+			 	while(choice) {
+			 	 System.out.println("Enter your command:(1 for edit and 2 for remove)");
+			 	 int command=sc.nextInt();
+			 	switch(command) {
+			 	  case 1:edit(l);
+			 	    break;
+			 	  case 2:remove(l);
+			 	    break;
+			 	  default: System.out.println("command not found");
+			 	    break;
+			 	 }
+			 	System.out.println("Do you want to cont for edit or remove..(y/n)");
+			 	String s=sc.next();
+			 	if(s.equals("n")) {
+			 		choice=false;
+			 		System.out.println("exit..");
+			 	}
+			 	}
+		}catch(Exception e) {
 			System.out.println(e);
 		}
+			
+	}
+	private static void remove(List l1) {
 		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("remove record");
+		 System.out.println("Enter name to remove:");
+		 String name=sc.next();
+		 int c=0;
+		for(int m=0;m<l1.size();m++){
+			AddressBook addressbook=(AddressBook) l1.get(m);
+			 if(name.equals(addressbook.firstname)) {
+				 l1.remove(addressbook);
+				 System.out.println("List After removing"+l1.toString());
+				 c++;
+				 break;
+				 
+			 }
+		}
+		if(c==0) {
+			System.out.println("User not found");
+		}
+	}
+	private static void edit(List l1) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Edit record");
+		 System.out.println("Enter name to edit:");
+		 String name=sc.next();
+		 int c=0;
+		for(int m=0;m<l1.size();m++){
+			AddressBook	addressbook=(AddressBook) l1.get(m);
+			 if(name.equals(addressbook.firstname)) {
+				 boolean choice=true;
+				 	while(choice) {
+				 	 System.out.println("Which data you want to edit ::(city,state,zip,phone,email) ");
+				 	String command=sc.next();
+				 	switch(command) {
+				 	  case "city":System.out.println("Enter new city:");
+					        addressbook.setCity(sc.next());
+				 	  break;
+				 	  case "state":System.out.println("Enter new state:");
+					        addressbook.setState(sc.next());
+				 	  break;
+				 	  case "zip":System.out.println("Enter new zip:");
+					        addressbook.setZip(sc.next());
+			 	      break;
+				 	  case "phone":System.out.println("Enter new phone:");
+					        addressbook.setPhoneNumber(sc.next());
+			 	      break;
+				 	  case "email":System.out.println("Enter new email:");
+			                addressbook.setEmail(sc.next());
+			 	      break;
+				 	  default: System.out.println("command not found");
+				 	  break;
+				 	 }
+				 	System.out.println("Do you want to any other data..(y/n)");
+				 	String s=sc.next();
+				 	if(s.equals("n")) {
+				 		choice=false;
+				 		System.out.println("exit..");
+				 	}
+				 	}
+			
+				 System.out.println("List After editing is"+l1.toString());
+				 c++;
+				 break;
+			 }
+		}
+		if(c==0) {
+			System.out.println("User not found");
+		}
 	}
 }
